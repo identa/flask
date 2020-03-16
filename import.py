a7a3421c9c847c3ca01284ad9f610b02182c12de
tatrunganh1996 <tatrunganha28@gmail.com>
2020-03-16 18:46:15 +0700

    remove redundant code

--- import.py
+++ import.py
@@ -3,21 +3,10 @@ from models import *
 from create import *
 from sqlalchemy.sql import text
 
-# app = Flask(__name__)
-# app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:root@localhost/spring"
-# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-# db.init_app(app)
-
 def importFlight():
     flight = Flight(origin="SG", destination="ENG", duration=10)
     db.session.add(flight)
     db.session.commit()
-
-
-# if __name__ == '__main__':
-#     with app.app_context():
-#         importFlight()
-
 
 @app.route('/import')
 def ip1():
