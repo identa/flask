df5f90807894d916c64bf9c2c77e1b89345ae1d3
Trung Anh <anh.tt@dactech.vn>
2020-03-16 17:09:13 +0700

    add textual sql execution

--- import.py
+++ import.py
@@ -1,24 +1,34 @@
 from flask import Flask, request
 from models import *
+from create import *
+from sqlalchemy.sql import text
 
+# app = Flask(__name__)
+# app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:root@localhost/spring"
+# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+# db.init_app(app)
 
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:root@localhost/spring"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db.init_app(app)
-
-def main():
+def importFlight():
     flight = Flight(origin="SG", destination="ENG", duration=10)
     db.session.add(flight)
     db.session.commit()
 
 
-if __name__ == '__main__':
-    with app.app_context():
-        main()
+# if __name__ == '__main__':
+#     with app.app_context():
+#         importFlight()
 
 
 @app.route('/import')
-def hello_world():
-    main()
+def ip1():
+    importFlight()
     return "Hello world"
+
+@app.route('/text')
+def text1():
+    sql = text('select origin from Flights')
+    result = db.engine.execute(sql)
+
+    for row in result:
+        print(row[0])
+    return "Done!"
